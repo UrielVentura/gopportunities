200093fc8ddeaf459cd13b89df431d6a67c58626
Uriel Ventura <36737847+UrielVentura@users.noreply.github.com>
2024-09-18 13:33:13 -0400

    feat: add custom logger

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "github.com/UrielVentura/gopportunities/router"
+import (
+	"github.com/UrielVentura/gopportunities/config"
+	"github.com/UrielVentura/gopportunities/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	// Initialize Config
+	err := config.Init()
+	if err != nil {
+		logger.Errorf("config initialization error: %v", err)
+		return
+	}
 	// Initialize Router
 	router.Initialize()
 }
